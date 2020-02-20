03491ff12633ffe2b0c107ae01f4d001050b36ba
weinda <affe52@gmx.at>
2020-02-20 17:27:27 +0100

    adding Viewmodel and Database

--- Lesson-6/andfun-kotlin-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ Lesson-6/andfun-kotlin-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -23,7 +23,7 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 /**
  * ViewModel for SleepTrackerFragment.
  */
-class SleepTrackerViewModel(
+class   SleepTrackerViewModel(
         val database: SleepDatabaseDao,
         application: Application) : AndroidViewModel(application) {
 }
